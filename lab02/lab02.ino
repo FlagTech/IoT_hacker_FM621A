b099be75a6d97597eb89e703697d236731a691f7
SamsonWoof <garywang323@gmail.com>
2020-08-10 18:13:15 +0800

    更正lab02的範例程式

--- lab02/lab02.ino
+++ lab02/lab02.ino
@@ -1,39 +1,45 @@
-int pin = 2;  //定義 LED 燈腳位
-
+int pin = 2;                 // 定義 LED 燈腳位
+const int t_interval = 1000; // 單位間隔時間為 1000 毫秒
 void setup()
 {
-  Serial.begin(115200); 
-  pinMode(pin, OUTPUT);
+    Serial.begin(115200);
+    pinMode(pin, OUTPUT); // 設定 pin 腳位為輸出腳位
 }
-
+void dot() // 傳送"點"
+{
+    digitalWrite(pin, HIGH); // 亮起 LED 燈
+    delay(t_interval);
+    digitalWrite(pin, LOW);
+    delay(t_interval);
+}
+void dash() // 傳送"劃"
+{
+    digitalWrite(pin, HIGH);
+    delay(t_interval * 3);
+    digitalWrite(pin, LOW);
+    delay(t_interval);
+}
 void loop()
 {
-  Serial.println("F");  
-  dot(); dot(); dash(); dot(); //利用 2 種函式進行不同閃爍來傳送F
-  delay(3000);  //每個字母間隔 3000 毫秒
-  Serial.println("L");
-  dot(); dash(); dot(); dot(); //L
-  delay(3000);
-  Serial.println("A");
-  dot(); dash(); //A
-  delay(3000);
-  Serial.println("G");
-  dash(); dash();dot(); //G
-  delay(5000);
-}
-
-void dot()  //傳送"點"
-{
-  digitalWrite(pin, HIGH);  //亮起 LED 燈
-  delay(250);
-  digitalWrite(pin, LOW);
-  delay(250);
-}
-
-void dash() //傳送"劃"
-{
-  digitalWrite(pin, HIGH);
-  delay(1000);
-  digitalWrite(pin, LOW);
-  delay(250);
+    Serial.println("F");
+    dot();
+    dot();
+    dash();
+    dot();                 // 利用 2 種函式進行不同閃爍來表示 F
+    delay(t_interval * 3); // 每個字母間隔 3000 毫秒
+    Serial.println("L");
+    dot();
+    dash();
+    dot();
+    dot(); // L
+    delay(t_interval * 3);
+    Serial.println("A");
+    dot();
+    dash(); // A
+    delay(t_interval * 3);
+    Serial.println("G");
+    dash();
+    dash();
+    dot(); // G
+    delay(t_interval * 7);
 }
